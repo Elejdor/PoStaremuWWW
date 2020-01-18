268f0adf43b1cbbf5d17809e184c1cd5fb75e285
elejdor <elejdor@gmail.com>
2020-01-18 10:40:57 +0100

    Pokrzyk

--- news.php
+++ news.php
@@ -1,33 +1,17 @@
 <div id="tiles">
 <div class="tile" 
-day="20" month="X" time="16:00" venue="ZPiT Harnam"
-title="Potańc Po Staremu // łęczycko-łowicki" 
-url="https://www.facebook.com/events/1818723784937792/" 
-img="https://scontent-frt3-2.xx.fbcdn.net/v/t1.0-9/72106185_3025970130810430_1193417827822862336_o.jpg?_nc_cat=111&_nc_oc=AQm_8kJiIg8TjIYCH0Oyt0gKBXZmtb7OVrcah39so2YLtXMDYbTCf_LiKlGT8zz3hFM&_nc_ht=scontent-frt3-2.xx&oh=925b12f14ecfd76217bfaff3be392936&oe=5E19890F"
-desc="Potańcówka z kapelami ludowymi. Mistrzowie tradycji zagrają w naturalnej, imprezowej sytuacji. Wstęp free.">
-</div>
-  
-<div class="tile" 
-day="23" month="X" time="18:00" venue="Teatr Szwalnia"
-title="Środy Po Staremu" 
-url="https://www.facebook.com/pg/fundacja.postaremu/events/?ref=page_internal" 
-img="https://scontent-frt3-2.xx.fbcdn.net/v/t1.0-9/53478289_2469678709772911_8312942448749838336_o.jpg?_nc_cat=105&_nc_oc=AQn4vOlzfxrf1jFW6KaT0BrifVyG92Q4jHRgZthn2Ha8kv4eV_CqsdZPwv87e-b3c5g&_nc_ht=scontent-frt3-2.xx&oh=2c3104924bfea3577be30c94957408c6&oe=5E5E2980"
-desc="Nauka śpiewu i tańca tradycyjnego od podstaw.">
+day="19" month="I" time="17:00" venue="TU"
+title="Otwarte warsztaty śpiewu tradycyjnego // Pokrzyk" 
+url="https://www.facebook.com/events/3498185286919174/"
+img="https://scontent-frx5-1.xx.fbcdn.net/v/t1.0-9/81882815_3318020078272099_5745546604203474944_o.jpg?_nc_cat=100&_nc_ohc=m48xfp9zkwYAX-f7l9j&_nc_ht=scontent-frx5-1.xx&oh=2fd8913c8fefe4727ca72fd05134bc87&oe=5E91D497"
+desc="Warsztaty śpiewu kapelą Pokrzyk.">
 </div>
 
 <div class="tile" 
-day="30" month="X" time="18:00" venue="Teatr Szwalnia"
-title="Środy Po Staremu" 
-url="https://www.facebook.com/pg/fundacja.postaremu/events/?ref=page_internal" 
-img="https://scontent-frt3-2.xx.fbcdn.net/v/t1.0-9/53478289_2469678709772911_8312942448749838336_o.jpg?_nc_cat=105&_nc_oc=AQn4vOlzfxrf1jFW6KaT0BrifVyG92Q4jHRgZthn2Ha8kv4eV_CqsdZPwv87e-b3c5g&_nc_ht=scontent-frt3-2.xx&oh=2c3104924bfea3577be30c94957408c6&oe=5E5E2980"
-desc="Nauka śpiewu i tańca tradycyjnego od podstaw.">
-</div>
-
-<div class="tile" 
-day="17" month="XI" time="18:00" venue=""
-title="Prusinowski Trio" 
-url="https://www.facebook.com/events/3328196780531565/" 
-img="https://scontent-frt3-2.xx.fbcdn.net/v/t1.0-9/70961509_2984505631623547_3939099371143954432_n.jpg?_nc_cat=109&_nc_oc=AQlCXCsbd4qp7v2TVfyMBf1-IxENCfGw7GdgcJIzTCWfPZiv9B8KaoL6bC2ss24HtH4&_nc_ht=scontent-frt3-2.xx&oh=1ea6551b9019364adf11bd582e3dc06e&oe=5E22103A"
-desc="Impreza z jednymi z najlepszych muzyków tradycyjnych w Polsce.">
+day="19" month="I" time="19:00" venue="TU"
+title="Potańc Po Staremu // Pokrzyk" 
+url="https://www.facebook.com/events/2609089062644235/"
+img="https://scontent-frt3-1.xx.fbcdn.net/v/t1.0-9/79424446_3235317336542374_6487530899263979520_n.jpg?_nc_cat=104&_nc_ohc=aY_inZcAZN8AX-8eAs7&_nc_ht=scontent-frt3-1.xx&oh=e2208a65f3194d8fe01e0b367a84f99f&oe=5E9E7EAB"
+desc="Warsztaty śpiewu, tańca i potańcówka z kapelą Pokrzyk.">
 </div>
 </div>
